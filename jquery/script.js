346e30e5e9b0f18b95a4263bae5232c2f6cd0af2
Anwesh Budhathoki <abperni@gmail.com>
2021-11-30 06:57:02 +0545

    JQuery Day2

--- jquery/script.js
+++ jquery/script.js
@@ -12,5 +12,51 @@ $(document).ready(() => {
             $('#hide').text('Hide');
             toggle = !toggle;
         });
-    })
+    });
+
+    $('#fadeout').click(() => {
+        $('.main').fadeOut(3000);
+    });
+
+    $('#fadein').click(() => {
+        $('.main').fadeIn(300);
+    });
+
+    $('#slideup').click(() => {
+        $('.main').slideUp(300);
+    });
+
+    $('#slidedown').click(() => {
+        $('.main').slideDown(300);
+    });
+
+    $('#fadetoggle').click(() => {
+        $('.main').fadeToggle(300);
+    });
+
+    $('#slidetoggle').click(() => {
+        $('.main').slideToggle(300);
+    });
+
+    const mathi = () => {
+        $('.main').slideUp(3000,tala);       
+    }
+
+    const tala = () => {
+        $('.main').slideDown(3000,mathi);       
+    }
+
+    $('#ghumne').click(()=>{
+        $('.main').slideUp(3000,tala)       
+    });
+
+    $('#append').click(() => {
+        $('.main').append(`<img src='./anwesh_id.jpg' height="50" width="50"/>`);
+    });
+
+    $('#prepend').click(() => {
+        $('.main').prepend('<b>Thik xa hauu</b>');
+    });
+
+
 });
